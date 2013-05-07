a030d905c50200eb107229c6800f0b12f330d746
mmutzell <mmutzell@kth.se>
2013-05-07 21:30:38 +0200

    added Game, containing main class. Also claryfied how GUI interacts with the other classes

---
+++ src/Game.java
@@ -0,0 +1,20 @@
+
+public class Game {
+	
+	private static Player[] players;
+	private static GUI home;
+	private static Pie[][] map;
+	private static int size;
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		size = 5;
+		//map = Initiate.generateMap(size);
+		home = new GUI(map, size, players.length);
+
+	}
+	
+
+}
